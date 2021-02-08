6d1081b411b25f5d3e6515d2f2c642b9f1e83b36
dthan <xopxo@DESKTOP-RA6LRPA>
2021-02-08 13:03:17 +0200

    Finished and Submitted

--- Homework/OrderCreatorService.cs
+++ Homework/OrderCreatorService.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
 using System.Linq;
 using Homework.Models;
 
@@ -8,117 +6,122 @@ namespace Homework
 {
     public class OrderCreatorService
     {
-        public const int DefaultBatchSize = 1;
-        public const string DefaultGeneratedCode = "BS_GENERATED_";
-        public const int DefaultQuantity = 1;
+        private const int DefaultBatchSize = 1;
+        private const string DefaultGeneratedCode = "BS_GENERATED_";
+        private const int DefaultQuantity = 1;
 
         public List<Order> CreateOrder(List<Product> ProductList, List<BatchSize> BatchSizeList, List<ProductBatchSize> ProductBatchSizeList, List<BatchQuantity> BatchQuantityList, bool Selection)
         {
-            var OrderList = new List<Order>();
+            var orderList = new List<Order>();
             
-            foreach(var Product in ProductList)
+            foreach(var product in ProductList)
             {
-                var Order = new Order();
-                Order.ProductCode = Product.Code;
-                Order.ProductName = Product.Name;
-                Order.BatchSizeCode = FindBatchSizeCode(Product.Code, BatchSizeList, ProductBatchSizeList, Selection);
-                Order.BatchSize = FindBatchSize(Order.BatchSizeCode, BatchSizeList);
-                Order.BatchQuantity = FindBatchQuantity(Product.Code, BatchQuantityList);
-                Order.Price = Product.Price;
-                OrderList.Add(Order);
+                var order = new Order();
+                order.ProductCode = product.Code;
+                order.ProductName = product.Name;
+                order.BatchSizeCode = FindBatchSizeCode(product.Code, BatchSizeList, ProductBatchSizeList, Selection);
+                order.BatchSize = FindBatchSize(order.BatchSizeCode, BatchSizeList);
+                order.BatchQuantity = FindBatchQuantity(product.Code, BatchQuantityList);
+                order.Price = product.Price;
+                orderList.Add(order);
             }
-            return OrderList;
+            return orderList;
         }
 
         private string FindBatchSizeCode(string ProductCode, List<BatchSize> BatchSizeList, List<ProductBatchSize> ProductBatchSizeList, bool Selection)
         {
-            var SelectedBatchSizeCodeList = FindSelectedBatchSizeCodeList(ProductCode, BatchSizeList, ProductBatchSizeList);
+            var selectedBatchSizeCodeList = FindSelectedBatchSizeCodeList(ProductCode, BatchSizeList, ProductBatchSizeList);
 
-            if (SelectedBatchSizeCodeList.Count == 0)
+            if (selectedBatchSizeCodeList.Count == 0)
+            {
                 return DefaultGeneratedCode + ProductCode;
-            else if (SelectedBatchSizeCodeList.Count == 1)
-                return SelectedBatchSizeCodeList.First().Code;
+            }
+            else if (selectedBatchSizeCodeList.Count == 1)
+            {
+                return selectedBatchSizeCodeList.First().Code;
+            }
             if (Selection == true)
-                return FindMaximumBatchSizeCode(SelectedBatchSizeCodeList);
-            return FindMinimumBatchSizeCode(SelectedBatchSizeCodeList);
+            {
+                return FindMaximumBatchSizeCode(selectedBatchSizeCodeList);
+            }
+            return FindMinimumBatchSizeCode(selectedBatchSizeCodeList);
         }
 
         private List<BatchSize> FindSelectedBatchSizeCodeList(string ProductCode, List<BatchSize> BatchSizeList, List<ProductBatchSize> ProductBatchSizeList)
         {
-            var SelectedBatchSizeCodeList = new List<BatchSize>();
+            var selectedBatchSizeCodeList = new List<BatchSize>();
 
-            foreach (var ProductBatchSize in ProductBatchSizeList)
+            foreach (var productBatchSize in ProductBatchSizeList)
             {
-                if (ProductCode.Equals(ProductBatchSize.ProductCode))
+                if (ProductCode.Equals(productBatchSize.ProductCode))
                 {
-                    foreach (var BatchSize in BatchSizeList)
+                    foreach (var batchSize in BatchSizeList)
                     {
-                        if (ProductBatchSize.BatchSizeCode.Equals(BatchSize.Code))
+                        if (productBatchSize.BatchSizeCode.Equals(batchSize.Code))
                         {
-                            SelectedBatchSizeCodeList.Add(BatchSize);
+                            selectedBatchSizeCodeList.Add(batchSize);
                             break;
                         }
                     }
                 }
             }
-            return SelectedBatchSizeCodeList;
+            return selectedBatchSizeCodeList;
         }
 
         private string FindMaximumBatchSizeCode(List<BatchSize> SelectedBatchSizeList)
         {
-            var Batch = SelectedBatchSizeList.First();
-            var Max = Batch.Size;
+            var batch = SelectedBatchSizeList.First();
+            var max = batch.Size;
 
-            foreach (var SelectedBatch in SelectedBatchSizeList)
+            foreach (var selectedBatch in SelectedBatchSizeList)
             {
-                if (Max < SelectedBatch.Size)
+                if (max < selectedBatch.Size)
                 {
-                    Batch = SelectedBatch;
-                    Max = Batch.Size;
+                    batch = selectedBatch;
+                    max = batch.Size;
                 }
             }
-            return Batch.Code;
+            return batch.Code;
         }
 
         private string FindMinimumBatchSizeCode(List<BatchSize> SelectedBatchSizeList)
         {
-            var Batch = SelectedBatchSizeList.First();
-            var Min = Batch.Size;
+            var batch = SelectedBatchSizeList.First();
+            var min = batch.Size;
 
-            foreach (var SelectedBatch in SelectedBatchSizeList)
+            foreach (var selectedBatch in SelectedBatchSizeList)
             {
-                if (Min > SelectedBatch.Size)
+                if (min > selectedBatch.Size)
                 {
-                    Batch = SelectedBatch;
-                    Min = Batch.Size;
+                    batch = selectedBatch;
+                    min = batch.Size;
                 }
             }
-            return Batch.Code;
+            return batch.Code;
         }
 
         private int FindBatchSize(string Code, List<BatchSize> BatchSizeList)
         {
-            foreach (var Batch in BatchSizeList)
-                if (Code.Equals(Batch.Code))
-                    return Batch.Size;
+            foreach (var batch in BatchSizeList)
+            {
+                if (Code.Equals(batch.Code))
+                {
+                    return batch.Size;
+                }
+            }
             return DefaultBatchSize;
         }
 
         private int FindBatchQuantity(string ProductCode, List<BatchQuantity> BatchQuantityList)
         {
-            foreach (var Item in BatchQuantityList)
-                if (ProductCode.Equals(Item.ProductCode))
-                    return (Item.Quantity);
+            foreach (var item in BatchQuantityList)
+            {
+                if (ProductCode.Equals(item.ProductCode))
+                {
+                    return (item.Quantity);
+                }
+            }
             return DefaultQuantity;
-        }
-        public string ToString(List<Order> OrderList)
-        {
-            var Sb = new StringBuilder();
-
-            Sb.AppendLine("Orders");
-            foreach (var Order in OrderList)
-                Sb.AppendLine(String.Format("|{0,-20}|{1,-20}|{2,-20}|{3,20}|{4,20}|{5,-20}|", Order.ProductCode, Order.ProductName, Order.BatchSizeCode, Order.BatchSize, Order.BatchQuantity, Order.Price));
-            return Sb.ToString();
         }
     }
 }
--- Homework/Program.cs
+++ Homework/Program.cs
@@ -1,9 +1,123 @@
-﻿namespace Homework
+﻿using Homework.Models;
+using System.Collections.Generic;
+using System.Text;
+using System;
+
+namespace Homework
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Max Batch Size Selection");
+            MaxBatchSize();
+            Console.WriteLine("Min Batch Size Selection");
+            MinBatchSize();
+        }
+        public static void MaxBatchSize()
+        {
+            var productList = new List<Product>();
+            productList.Add(new Product { Code = "P1", Name = "Milk", Price = 1.99f });
+            productList.Add(new Product { Code = "P2", Name = "Sour Milk", Price = 2.05f });
+            productList.Add(new Product { Code = "P3", Name = "Cream", Price = 3.59f });
+            productList.Add(new Product { Code = "P4", Name = "Yoghurt", Price = 4.99f });
+            productList.Add(new Product { Code = "P5", Name = "Buttermilk", Price = 3.1f });
+
+            var batchSizeList = new List<BatchSize>();
+            batchSizeList.Add(new BatchSize { Code = "BS1", Size = 20 });
+            batchSizeList.Add(new BatchSize { Code = "BS2", Size = 30 });
+            batchSizeList.Add(new BatchSize { Code = "BS3", Size = 40 });
+            batchSizeList.Add(new BatchSize { Code = "BS4", Size = 50 });
+            batchSizeList.Add(new BatchSize { Code = "BS5", Size = 100 });
+            batchSizeList.Add(new BatchSize { Code = "BS6", Size = 20 });
+            batchSizeList.Add(new BatchSize { Code = "BS7", Size = 50 });
+
+            var productBatchSizeList = new List<ProductBatchSize>();
+            productBatchSizeList.Add(new ProductBatchSize { ProductCode = "P1", BatchSizeCode = "BS6" });
+            productBatchSizeList.Add(new ProductBatchSize { ProductCode = "P2", BatchSizeCode = "BS1" });
+            productBatchSizeList.Add(new ProductBatchSize { ProductCode = "P2", BatchSizeCode = "BS2" });
+            productBatchSizeList.Add(new ProductBatchSize { ProductCode = "P2", BatchSizeCode = "BS3" });
+            productBatchSizeList.Add(new ProductBatchSize { ProductCode = "P3", BatchSizeCode = "BS4" });
+            productBatchSizeList.Add(new ProductBatchSize { ProductCode = "P3", BatchSizeCode = "BS5" });
+            productBatchSizeList.Add(new ProductBatchSize { ProductCode = "P5", BatchSizeCode = "BS7" });
+
+            var batchQuantityList = new List<BatchQuantity>();
+            batchQuantityList.Add(new BatchQuantity { ProductCode = "P1", Quantity = 20 });
+            batchQuantityList.Add(new BatchQuantity { ProductCode = "P2", Quantity = 500 });
+            batchQuantityList.Add(new BatchQuantity { ProductCode = "P3", Quantity = 40 });
+            batchQuantityList.Add(new BatchQuantity { ProductCode = "P4", Quantity = 234 });
+
+            var result = new List<Order>();
+            result.Add(new Order { ProductCode = "P1", ProductName = "Milk", BatchSizeCode = "BS6", BatchSize = 20, BatchQuantity = 20, Price = 1.99f });
+            result.Add(new Order { ProductCode = "P2", ProductName = "Sour Milk", BatchSizeCode = "BS3", BatchSize = 40, BatchQuantity = 500, Price = 2.05f });
+            result.Add(new Order { ProductCode = "P3", ProductName = "Cream", BatchSizeCode = "BS5", BatchSize = 100, BatchQuantity = 40, Price = 3.59f });
+            result.Add(new Order { ProductCode = "P4", ProductName = "Yoghurt", BatchSizeCode = "BS_GENERATED_P4", BatchSize = 1, BatchQuantity = 234, Price = 4.99f });
+            result.Add(new Order { ProductCode = "P5", ProductName = "Buttermilk", BatchSizeCode = "BS7", BatchSize = 50, BatchQuantity = 1, Price = 3.1f });
+
+            var orderCreatorService = new OrderCreatorService();
+            var orderList = orderCreatorService.CreateOrder(productList, batchSizeList, productBatchSizeList, batchQuantityList, true);
+
+            Console.WriteLine("Expected Result");
+            Console.WriteLine(ToString(orderList));
+            Console.WriteLine("Actual Result");
+            Console.WriteLine(ToString(result));
+        }
+        public static void MinBatchSize()
+        {
+            var productList = new List<Product>();
+            productList.Add(new Product { Code = "P1", Name = "Milk", Price = 1.99f });
+            productList.Add(new Product { Code = "P2", Name = "Sour Milk", Price = 2.05f });
+            productList.Add(new Product { Code = "P3", Name = "Cream", Price = 3.59f });
+            productList.Add(new Product { Code = "P4", Name = "Yoghurt", Price = 4.99f });
+            productList.Add(new Product { Code = "P5", Name = "Buttermilk", Price = 3.1f });
+
+            var batchSizeList = new List<BatchSize>();
+            batchSizeList.Add(new BatchSize { Code = "BS1", Size = 20 });
+            batchSizeList.Add(new BatchSize { Code = "BS2", Size = 30 });
+            batchSizeList.Add(new BatchSize { Code = "BS3", Size = 40 });
+            batchSizeList.Add(new BatchSize { Code = "BS4", Size = 50 });
+            batchSizeList.Add(new BatchSize { Code = "BS5", Size = 100 });
+            batchSizeList.Add(new BatchSize { Code = "BS6", Size = 20 });
+            batchSizeList.Add(new BatchSize { Code = "BS7", Size = 50 });
+
+            var productBatchSizeList = new List<ProductBatchSize>();
+            productBatchSizeList.Add(new ProductBatchSize { ProductCode = "P1", BatchSizeCode = "BS6" });
+            productBatchSizeList.Add(new ProductBatchSize { ProductCode = "P2", BatchSizeCode = "BS1" });
+            productBatchSizeList.Add(new ProductBatchSize { ProductCode = "P2", BatchSizeCode = "BS2" });
+            productBatchSizeList.Add(new ProductBatchSize { ProductCode = "P2", BatchSizeCode = "BS3" });
+            productBatchSizeList.Add(new ProductBatchSize { ProductCode = "P3", BatchSizeCode = "BS4" });
+            productBatchSizeList.Add(new ProductBatchSize { ProductCode = "P3", BatchSizeCode = "BS5" });
+            productBatchSizeList.Add(new ProductBatchSize { ProductCode = "P5", BatchSizeCode = "BS7" });
+
+            var batchQuantityList = new List<BatchQuantity>();
+            batchQuantityList.Add(new BatchQuantity { ProductCode = "P1", Quantity = 20 });
+            batchQuantityList.Add(new BatchQuantity { ProductCode = "P2", Quantity = 500 });
+            batchQuantityList.Add(new BatchQuantity { ProductCode = "P3", Quantity = 40 });
+            batchQuantityList.Add(new BatchQuantity { ProductCode = "P4", Quantity = 234 });
+
+            var result = new List<Order>();
+            result.Add(new Order { ProductCode = "P1", ProductName = "Milk", BatchSizeCode = "BS6", BatchSize = 20, BatchQuantity = 20, Price = 1.99f });
+            result.Add(new Order { ProductCode = "P2", ProductName = "Sour Milk", BatchSizeCode = "BS1", BatchSize = 20, BatchQuantity = 500, Price = 2.05f });
+            result.Add(new Order { ProductCode = "P3", ProductName = "Cream", BatchSizeCode = "BS4", BatchSize = 50, BatchQuantity = 40, Price = 3.59f });
+            result.Add(new Order { ProductCode = "P4", ProductName = "Yoghurt", BatchSizeCode = "BS_GENERATED_P4", BatchSize = 1, BatchQuantity = 234, Price = 4.99f });
+            result.Add(new Order { ProductCode = "P5", ProductName = "Buttermilk", BatchSizeCode = "BS7", BatchSize = 50, BatchQuantity = 1, Price = 3.1f });
+
+            var orderCreatorService = new OrderCreatorService();
+            var orderList = orderCreatorService.CreateOrder(productList, batchSizeList, productBatchSizeList, batchQuantityList, false);
+
+            Console.WriteLine("Expected Result");
+            Console.WriteLine(ToString(orderList));
+            Console.WriteLine("Actual Result");
+            Console.WriteLine(ToString(result));
+        }
+        public static string ToString(List<Order> OrderList)
+        {
+            var sb = new StringBuilder();
+
+            sb.AppendLine("Orders");
+            foreach (var Order in OrderList)
+                sb.AppendLine(string.Format("|{0,-20}|{1,-20}|{2,-20}|{3,20}|{4,20}|{5,-20}|", Order.ProductCode, Order.ProductName, Order.BatchSizeCode, Order.BatchSize, Order.BatchQuantity, Order.Price));
+            return sb.ToString();
         }
     }
 }
